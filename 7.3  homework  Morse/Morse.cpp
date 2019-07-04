642d6aa11fa89ed88bbcb90b2298c0d68dc8d787
Shervin-211 <52454850+Shervin-211@users.noreply.github.com>
2019-07-04 21:54:10 +0800

    Add files via upload

---
+++ 7.3  homework  Morse/Morse.cpp
@@ -0,0 +1,170 @@
+#include "Arduino.h"
+#include "Morse.h"
+
+Morse::Morse(int pin)
+{
+	pinMode(pin,OUTPUT);
+	_pin=pin;
+	_dottime=250;
+}
+
+void Morse::dot()
+{
+	digitalWrite(_pin,HIGH);
+	delay(_dottime);
+	digitalWrite(_pin,LOW);
+	delay(_dottime);
+}
+
+void Morse::dash()
+{
+	digitalWrite(_pin,HIGH);
+	delay(_dottime*4);
+	digitalWrite(_pin,LOW);
+	delay(_dottime);
+}
+
+void Morse::c_space()
+{
+	digitalWrite(_pin,LOW);
+	delay(_dottime*3);
+}
+
+void Morse::w_space()
+{
+	digitalWrite(_pin,LOW);
+	delay(_dottime*7);
+}
+
+//∂®“Âa~z
+void Morse::T_a()
+{
+    dot();dash();
+}
+
+void Morse::T_b()
+{
+    dash();dot();dot();dot();
+}
+
+void Morse::T_c()
+{
+    dash();dot();dash();dot();
+}
+
+void Morse::T_d()
+{
+    dash();dot();dot();
+}
+
+void Morse::T_e()
+{
+    dot();
+}
+
+void Morse::T_f()
+{
+    dot();dot();dash();dot();
+}
+
+void Morse::T_g()
+{
+    dash();dash();dot();
+}
+
+void Morse::T_h()
+{
+    dot();dot();dot();dot();
+}
+
+void Morse::T_i()
+{
+    dot();dot();
+}
+
+void Morse::T_j()
+{
+    dot();dash();dash();dash();
+}
+
+void Morse::T_k()
+{
+    dot();dash();dot();
+}
+
+void Morse::T_l()
+{
+    dot();dash();dot();dot();
+}
+
+void Morse::T_m()
+{
+    dash();dash();
+}
+
+void Morse::T_n()
+{
+    dash();dot();
+}
+
+void Morse::T_o()
+{
+    dash();dash();dash();
+}
+
+void Morse::T_p()
+{
+    dot();dash();dash();dot();
+}
+
+void Morse::T_q()
+{
+    dash();dash();dot();dash();
+}
+
+void Morse::T_r()
+{
+    dot();dash();dot();
+}
+
+void Morse::T_s()
+{
+    dot();dot();dot();
+}
+
+void Morse::T_t()
+{
+    dash();
+}
+
+void Morse::T_u()
+{
+    dot();dot();dash();
+}
+
+void Morse::T_v()
+{
+    dot();dot();dot();dash();
+}
+
+void Morse::T_w()
+{
+    dot();dash();dash();
+}
+
+void Morse::T_x()
+{
+    dash();dot();dot();dash();
+}
+
+void Morse::T_y()
+{
+    dash();dot();dash();dash();
+}
+
+void Morse::T_z()
+{
+    dash();dash();dot();dot();
+}
+
+
---
+++ 7.3  homework  Morse/Morse.h
@@ -0,0 +1,41 @@
+#ifndef _MORSE_H
+#define _MORSE_H
+class Morse
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+    void c_space();
+    void w_space();
+    void T_a();
+    void T_b();
+    void T_c();
+    void T_d();
+    void T_e();
+    void T_f();
+    void T_g();
+    void T_h();
+    void T_i();
+    void T_j();
+    void T_k();
+    void T_l();
+    void T_m();
+    void T_n();
+    void T_o();
+    void T_p();
+    void T_q();
+    void T_r();
+    void T_s();
+    void T_t();
+    void T_u();
+    void T_v();
+    void T_w();
+    void T_x();
+    void T_y();
+    void T_z();
+  private:
+    int _pin;
+    int _dottime;
+};
+#endif /*_MORSE_H*/
